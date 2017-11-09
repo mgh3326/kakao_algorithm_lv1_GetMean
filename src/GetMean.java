6a6897549c21f08c0a119592b1311d2fb2f07344
mgh3326 <mgh3326@naver.com>
2017-11-10 08:11:09 +0900

    init2

---
+++ src/GetMean.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2017. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+public class GetMean {
+    public int getMean(int[] array) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int x[] = {5, 4, 3};
+        GetMean getMean = new GetMean();
+        // 아래는 테스트로 출력해 보기 위한 코드입니다.
+        System.out.println("평균값 : " + getMean.getMean(x));
+    }
+}
